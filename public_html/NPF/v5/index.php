830a59902fdbf15fc07454534e258948fb12a74d
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2014-10-15 16:53:30 +0400

    Bag ispravlen.Informaciay po ispravleniy http://dnzl.ru/view_post.php?id=245

--- public_html/NPF/v5/index.php
+++ public_html/NPF/v5/index.php
@@ -457,7 +457,10 @@
 																	GetDisplay( $Country, $_SESSION["groupOfProgram"], 'advParamBag' ,'1');?> 
 																	>
 															<div id = 'divInpAdvParamBag' <? if ($_SESSION['divInpAdvParamBag']) echo $_SESSION['divInpAdvParamBag'];?> >
-																<input id='inpAdvParamBag' name='inpAdvParamBag' class='inpAdvParam' type='checkbox' <?if ($_SESSION['drawInpAdvParamBag']){ echo $_SESSION['drawInpAdvParamBag'];}?>   <? if($_SESSION["inpAdvParamBag"] == 1) {echo 'checked ';} else{echo '';} foreach($_SESSION['CountriesArrays'] as $OneCountry)
+																<input id='inpAdvParamBag' name='inpAdvParamBag' class='inpAdvParam' type='checkbox' value ="bagag"  <?if ($_SESSION['drawInpAdvParamBag']) echo $_SESSION['drawInpAdvParamBag'];?>   <? 
+                                                                                                                                  if($_POST && in_array('bmw', $_POST['inpAdvParamBag']) )
+                                                                                                                                  {  echo 'checked';  }
+                                                                                                                                foreach($_SESSION['CountriesArrays'] as $OneCountry)
 																	{	if($_SESSION["CountryID"] == $OneCountry[0]){$Country=$OneCountry[2];}} GetDisplay( $Country, $_SESSION["groupOfProgram"], 'advParamBag' ,'0');?> style='margin-left:10px' onclick="clickParam(this)" />
 															</div>
 															<li class='li-none'>
