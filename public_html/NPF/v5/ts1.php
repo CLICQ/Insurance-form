4b7c7a5fc1ff1e31cab3b09af3f84e36ec0b01e7
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2014-10-16 20:24:55 +0400

    Poisk i ogidanie dopolnitrlnuh dannuh

--- public_html/NPF/v5/ts1.php
+++ public_html/NPF/v5/ts1.php
@@ -6,9 +6,9 @@ header($string);
 		$client = new SoapClient("http://213.33.168.45:8082/txlife.wsdl");
 		
 		// Посылка SOAP-запроса c получением результат
-                print_r($client->__getFunctions());
+               print_r($client->__getFunctions());
                 exit;
-		$result1 = $client->Ping();
+		$result1 = $client->Ping("ConfirmationID");
                 $result2 = $client->TXLife();
                 $result3 = $client->TXLifeTransmittal();
 		echo  $result1;
